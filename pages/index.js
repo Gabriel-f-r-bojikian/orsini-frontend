1f66874a54b5ffcd897365dac830171ff3800409
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2022-04-03 23:28:33 -0300

    CSS setup

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Header from "../components/Header/header"
+import Head from "next/head"
+import Orsini from "./orsini"
 
 export default function Home() {
   return (
-  <div>
-    <Header />
-    <h1>Hello World</h1>
-  </div>
+    <>
+      <Head>
+        <title>Fast Data Collection</title>
+        <link rel="icon" type="image/x-icon" href="logo_lprot.jfif" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Orsini />
+    </>
   )
 }
---
+++ pages/orsini.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header/header"
+
+export default function Orsini() {
+  return (
+  <div>
+    <Header />
+    <h1>Hello World</h1>
+  </div>
+  )
+}
