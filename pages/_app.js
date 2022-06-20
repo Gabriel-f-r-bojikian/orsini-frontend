4f22430cdb454682e88ad42817e62de46a072bca
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2022-06-20 20:18:34 -0300

    Created context element to pass down data

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,38 @@
 import "../styles/styles.scss"
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+// import { DataQueue } from "../classes/DataQueue";
 const io = require("socket.io-client");
 const socket = io("http://localhost:2999", {
   transports: ['websocket']
 });
+// const DataContext = require("../context/DataContext");
+// const DataQueue = require('../classes/DataQueue');
+
+export const DataContext = React.createContext();
 
 function MyApp({Component, pageProps}) {
-    useEffect(() => {
+  const maxDataQueueSize = 100;
+  // const dataQueue = new DataQueue(maxDataQueueSize);
+  const dataQueue = [];
+
+  useEffect(() => {
         console.log("Got here")
         socket.on('incoming message', (message) => {
-           console.log(message); 
+          console.log(message);
+           
+          if( dataQueue.length > maxDataQueueSize ) {
+            dataQueue.shift();
+          }
+
+          dataQueue.push(message);
         })
     }, [socket])
     
-    return<Component {...pageProps} />
+    return(
+      <DataContext.Provider value={dataQueue}>
+        <Component {...pageProps} />
+      </DataContext.Provider>
+    );
 }
 
 export default MyApp
