3edfc5381299d6fc1d7ebe8974f8762180e09af7
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2022-06-26 16:46:09 -0300

    Bugfix: Removed unused react context

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,8 @@
 import "../styles/styles.scss"
-import React, { useEffect, useState } from "react";
-
-export const DataContext = React.createContext();
 
 function MyApp({Component, pageProps}) {    
     return(
-      <DataContext.Provider value={dataQueue}>
         <Component {...pageProps} />
-      </DataContext.Provider>
     );
 }
 
